7019647f00163e743a28808546a86ec6d86d69cf
sumit.kumar <sumit.kumar@docquity.com>
2023-07-20 17:45:14 +0530

    feat: initial commit

---
+++ src/main/java/com/example/eren/ErenApplication.java
@@ -0,0 +1,13 @@
+package com.example.eren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eren/ErenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
